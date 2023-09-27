e2e8e5f0372878deadc211644a07f2fecc513b82
Froxss <burakyetis@gmail.com>
2023-09-27 03:26:47 +0300

    Sidebarı düzenledim-sayfa içeriğine hover ekledim sidebar açıldıgında contenti saga kaydırdım projeler iconuna rotate verdim js ile gorev ekledikten sonra flexboxta bozulmalar vardı "grid" kullanarak duzeltmeye calıstım..

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   const openSidebarButton = document.getElementById("openSidebar");
   const sidebar = document.getElementById("sidebar");
+  const mainApp = document.querySelector(".main_app");
 
   openSidebarButton.addEventListener("click", function () {
     sidebar.classList.toggle("sidebar_open");
+
+    if (sidebar.classList.contains("sidebar_open")) {
+      swipeRight();
+    } else {
+      resetSwipe();
+    }
   });
+
+  var swipeValue = 550;
+
+  function swipeRight() {
+    mainApp.style.transition = "margin-left 0.3s";
+    mainApp.style.marginLeft = swipeValue + "px";
+  }
+
+  function resetSwipe() {
+    mainApp.style.transition = "margin-left 0.3s";
+    mainApp.style.marginLeft = "25%";
+  }
 });
 
 const yeniGorevButon = document.getElementById("yeniGorevButon");
@@ -14,7 +33,7 @@ const gorevListesi = document.getElementById("task_list");
 
 function yeniGorevEkle(gorevIsmi, gorevAciklamasi, gorevTarihi) {
   const yeniGorev = document.createElement("li");
-  yeniGorev.className = "body_container yeni-gorev"; // Yeni sınıf ekleyin
+  yeniGorev.className = "body_container yeni-gorev";
   yeniGorev.draggable = true;
   yeniGorev.innerHTML = `
   <div class="card_item_container">
@@ -23,12 +42,12 @@ function yeniGorevEkle(gorevIsmi, gorevAciklamasi, gorevTarihi) {
         <input type="checkbox" />
         <span class="checkmark"></span>
       </label>
-      <i class="fa-solid fa-grip-vertical fa-lg gripbutton" style="color: #464d589d; padding-left: 5px"></i>
+      
     </div>
     <div class="card_items_center">
-      <div class="gorev_ismi">${gorevIsmi}</div>
-      <div class="gorev_aciklamasi">${gorevAciklamasi}</div>
-      <div class="gorev_tarihi">${gorevTarihi}</div>
+      <div class="gorev_ismi"><p  class="cardContent">${gorevIsmi}</p></div>
+      <div class="gorev_aciklamasi"><p  class="cardContent">${gorevAciklamasi}</p></div>
+      <div class="gorev_tarihi"><p  class="cardContent">${gorevTarihi}</p></div>
     </div>
     <div class="card_items_right">
       <i class="fa-solid fa-trash-can fa-lg" style="color: #464d589d"></i>
@@ -39,16 +58,19 @@ function yeniGorevEkle(gorevIsmi, gorevAciklamasi, gorevTarihi) {
 
   const gorevListesi = document.getElementById("task_list");
   gorevListesi.appendChild(yeniGorev);
+  yeniGorevButon.innerText = "Yeni Görev Ekle";
 }
 
 yeniGorevButon.addEventListener("click", () => {
-  if (popup.style.display == "flex") {
-    popup.style.display = "none";
-    yeniGorevButon.innerText = "Yeni Görev Ekle";
-  } else {
-    popup.style.display = "flex";
+  popup.classList.toggle("active");
+  if (popup.classList.contains("active")) {
     yeniGorevButon.innerText = "Görev eklemekten vazgeç";
+    popup.style.display = "flex";
+  } else {
+    yeniGorevButon.innerText = "Yeni Görev Ekle";
+    popup.style.display = "none";
   }
+  popup.style.display = "flex";
 });
 
 document.getElementById("addTask").addEventListener("click", () => {
@@ -67,4 +89,16 @@ document.getElementById("addTask").addEventListener("click", () => {
   } else {
     alert("Lütfen tüm alanları doldurun!");
   }
+});
+
+const rotateIcon = document.getElementById("chevron_left_icon");
+let rotation = 0;
+
+rotateIcon.addEventListener("click", function () {
+  if (rotation == 0) {
+    rotation += -90;
+  } else {
+    rotation -= -90;
+  }
+  rotateIcon.style.transform = `rotate(${rotation}deg)`;
 });
